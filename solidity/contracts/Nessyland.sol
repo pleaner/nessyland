8cec314ddf43c30b0b0c1985deb47301d9d91da8
Sean Pleaner <sean@apexcrm.co.za>
2023-05-07 23:32:51 +0200

    Read Article Logic + tests

--- solidity/contracts/Nessyland.sol
+++ solidity/contracts/Nessyland.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 error Nessyland__UnauthorisedContentRead(uint tokenId);
 error Nessyland__NotEngoughPayment(uint price, uint paid);
+error Nessyland__ArticleDoesNotExist(uint tokenId);
 
 contract Nessyland is ERC721 {
     // Events
@@ -48,11 +49,20 @@ contract Nessyland is ERC721 {
     // Read Features
     mapping(uint256 => mapping(address => bool)) private s_tokenIdToContentPermission;
     mapping(address => uint) s_balanceDue;
+    uint private earnedCommision;
 
     // Modifiers
     modifier canReadContent(uint _tokenId) {
         if (!hasArticleReadPermission(_tokenId)) {
             revert Nessyland__UnauthorisedContentRead(_tokenId);
+        }
+        _;
+    }
+
+    modifier articleExistist(uint _tokenId) {
+        address author = s_tokenIdToArticle[_tokenId].author;
+        if (author == address(0)) {
+            revert Nessyland__ArticleDoesNotExist(_tokenId);
         }
         _;
     }
@@ -78,6 +88,7 @@ contract Nessyland is ERC721 {
         emit Published(msg.sender, _catagory, _subcatagory, _title, _description, _price);
 
         s_authorToArticleIds[msg.sender].push(s_tokenCounter);
+        grantContentPermission(msg.sender, s_tokenCounter); // can read own articles
 
         s_tokenCounter++;
         return s_tokenCounter;
@@ -87,18 +98,19 @@ contract Nessyland is ERC721 {
     //     return "TokenUrl";
     // }
 
-    // TODO Transfer Article
-
-    // Buy Right to Read Article
-    function purchaseRightToContent(uint _tokenId) public payable returns (uint) {
+    function purchaseRightToContent(
+        uint _tokenId
+    ) public payable articleExistist(_tokenId) returns (uint) {
         Article memory atricle = s_tokenIdToArticle[_tokenId];
 
         if (msg.value < atricle.price) {
             revert Nessyland__NotEngoughPayment(atricle.price, msg.value);
         }
 
-        s_balanceDue[_ownerOf(_tokenId)] += msg.value * (1 - (FEE_PERCENTAGE / 100));
+        s_balanceDue[_ownerOf(_tokenId)] += getPriceLessComission(msg.value);
+        s_balanceDue[address(this)] += getComission(msg.value);
         grantContentPermission(msg.sender, _tokenId);
+
         emit PurchaseRightToRead(
             atricle.author,
             _ownerOf(_tokenId),
@@ -108,14 +120,15 @@ contract Nessyland is ERC721 {
             atricle.price,
             block.timestamp
         );
+
         return _tokenId;
     }
 
-    function grantContentPermission(address _reader, uint _tokenId) internal {
+    function grantContentPermission(address _reader, uint _tokenId) private {
         s_tokenIdToContentPermission[_tokenId][_reader] = true;
     }
 
-    // Read Article
+    // TODO Withdrawl Balance Due
 
     // View Functions
     function getTokenCounter() external view returns (uint256) {
@@ -137,7 +150,33 @@ contract Nessyland is ERC721 {
         return result;
     }
 
+    function getArticleContent(
+        uint _tokenId
+    ) public view canReadContent(_tokenId) returns (string memory) {
+        return s_tokenIdToContent[_tokenId];
+    }
+
     function hasArticleReadPermission(uint _tokeId) public view returns (bool) {
         return s_tokenIdToContentPermission[_tokeId][msg.sender];
+    }
+
+    function getFeePercentage() public view returns (uint) {
+        return FEE_PERCENTAGE;
+    }
+
+    function getAmountDue() public view returns (uint) {
+        return s_balanceDue[msg.sender];
+    }
+
+    function getEarnedCommision() public view returns (uint) {
+        return earnedCommision;
+    }
+
+    function getPriceLessComission(uint price) public view returns (uint) {
+        return (price * (100 - FEE_PERCENTAGE)) / 100;
+    }
+
+    function getComission(uint price) public view returns (uint) {
+        return (price * FEE_PERCENTAGE) / 100;
     }
 }
--- solidity/test/NesslylandTest.js
+++ solidity/test/NesslylandTest.js
@@ -1,25 +1,28 @@
 const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")   
-const { time } = require("@nomicfoundation/hardhat-network-helpers");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { BigNumber } = require("ethers");
+
+const parseEther = ethers.utils.parseEther;
 
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Nessyland Unit Tests", function () {
-          let nessyland, deployer
 
-          beforeEach(async () => {
-              accounts = await ethers.getSigners()
-              deployer = accounts[0]
-              await deployments.fixture(["nessyland"])
-              nessyland = await ethers.getContract("Nessyland")
-          })
+        async function deployTokenFixture() {
+            const [deployer, reader, buyer, author] = await ethers.getSigners()
+            await deployments.fixture(["nessyland"])
+            nessyland = await ethers.getContract("Nessyland")
+            return {nessyland, deployer, reader, buyer, author}
+        }
         
         describe("Constructor", () => {
             it("Initializes the NFT Correctly.", async () => {
+                const { nessyland, owner } = await loadFixture(deployTokenFixture);
                 const name = await nessyland.name()
                 const symbol = await nessyland.symbol()
-                const tokenCounter=await nessyland.getTokenCounter()
+                const tokenCounter = await nessyland.getTokenCounter()
                 assert.equal(name, "Nessyland")
                 assert.equal(symbol, "NSYLD")
                 assert.equal(tokenCounter.toString(),"0")
@@ -28,13 +31,15 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
         // Test Publish
         describe("Publish Article", () => {
-          beforeEach(async () => {
+
+          async function publishArticlesFixture() {
+            const { nessyland, deployer, reader, buyer } = await loadFixture(deployTokenFixture);
               const txResponse1 = await nessyland.mintArticle(
                 "Atricle Title One",
                 "Article Description One",
                 "Catagory One",
                 "Sub Catagatgory One",
-                3,
+                parseEther("0.03"),
                 "Article One Content"
               )
               await txResponse1.wait(1)
@@ -43,12 +48,15 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
                 "Article Description Two",
                 "Catagory Two",
                 "Sub Catagatgory Two",
-                5,
+                parseEther("0.05"),
                 "Article Two Content"
               )
               await txResponse2.wait(1)
-          })
+              return {nessyland, deployer, reader, buyer}
+            }
+          
           it("Allows users to Publish an Article NFT, and updates appropriately", async function () {
+            const { nessyland } = await loadFixture(publishArticlesFixture);
             // TODO  const tokenURI = await nessyland.tokenURI(0)
               const tokenCounter = await nessyland.getTokenCounter()
 
@@ -56,6 +64,7 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
             //   assert.equal(tokenURI, await nessyland.TOKEN_URI())
           })
           it("Show the correct balance and owner of an NFT", async function () {
+            const { nessyland, deployer } = await loadFixture(publishArticlesFixture);
               const deployerAddress = deployer.address;
               const deployerBalance = await nessyland.balanceOf(deployerAddress)
               const owner = await nessyland.ownerOf("0")
@@ -64,6 +73,7 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
               assert.equal(owner, deployerAddress)
           })
           it("Fetch the correct metadata of NFT Article", async function () {
+            const { nessyland, deployer } = await loadFixture(publishArticlesFixture);
             const deployerAddress = deployer.address;
             const article = await nessyland.getArticleMetadata("0")
 
@@ -71,9 +81,10 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
             assert.equal(article.description, "Article Description One")
             expect(article.published.toString()).not.to.be.an('undefined')
             assert.equal(article.author, deployerAddress)
-            assert.equal(article.price.toString(), "3")
+            assert.equal(article.price.toString(), "30000000000000000")
           })
           it("Fetches the correct articles of author", async function () {
+            const { nessyland, deployer } = await loadFixture(publishArticlesFixture);
             const deployerAddress = deployer.address;
             const articles = await nessyland.getArticlesByAuthor(deployerAddress)
 
@@ -81,15 +92,16 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
             assert.equal(articles[0].description, "Article Description One")
             expect(articles[0].published.toString()).not.to.be.an('undefined')
             assert.equal(articles[0].author, deployerAddress)
-            assert.equal(articles[0].price.toString(), "3")
+            assert.equal(articles[0].price.toString(), "30000000000000000")
 
             assert.equal(articles[1].title, "Atricle Title Two")
             assert.equal(articles[1].description, "Article Description Two")
             expect(articles[1].published.toString()).not.to.be.an('undefined')
             assert.equal(articles[1].author, deployerAddress)
-            assert.equal(articles[1].price.toString(), "5")
+            assert.equal(articles[1].price.toString(), "50000000000000000")
           })
           it("Emit a Published event", async function(){
+            const { nessyland, deployer } = await loadFixture(publishArticlesFixture);
             const deployerAddress = deployer.address;
 
             await expect(await nessyland.mintArticle(
@@ -97,7 +109,7 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
                 "Article Description Three",
                 "Catagory Three",
                 "Sub Catagatgory Three",
-                7,
+                parseEther("0.07"),
                 "Article Three Content"
               ))
                 .to.emit(nessyland, "Published")
@@ -107,7 +119,96 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
                     "Sub Catagatgory Three",
                     "Atricle Title Three",
                     "Article Description Three",
-                    7);
+                    "70000000000000000");
           })
         })
+
+        // Test Read Permission
+        describe("Read Article", () => {
+
+            async function readArticleFixture() {
+                const { nessyland, deployer, reader, buyer, author } = await loadFixture(deployTokenFixture);
+                const nessylandAuthor = nessyland.connect(author)
+                const txResponse = await nessylandAuthor.mintArticle(
+                    "Atricle Title One",
+                    "Article Description One",
+                    "Catagory One",
+                    "Sub Catagatgory One",
+                    parseEther("0.01"),
+                    "Article One Content"
+                  )
+                  const receipt =  await txResponse.wait(1)
+                  const gasSpentAthor = receipt.gasUsed.mul(receipt.effectiveGasPrice)
+                  const nessylandReader = nessyland.connect(reader)
+                  return {nessyland, nessylandReader, deployer, reader, author, gasSpentAthor}
+                }
+            
+            it("Does not allow non reader to read", async function () {
+                const {nessylandReader} = await loadFixture(readArticleFixture);
+                const hasPermission = await nessylandReader.hasArticleReadPermission("0");
+                assert.isFalse(hasPermission)
+            })
+            it("Reverts when reader doesnt pay enough", async function () {
+                const {nessylandReader} = await loadFixture(readArticleFixture);
+                await expect(nessylandReader.purchaseRightToContent("0")).to.be.revertedWith( "Nessyland__NotEngoughPayment")
+            })
+            it("Reverts if article doesnt exists", async function () {
+                const {nessylandReader} = await loadFixture(readArticleFixture);
+                await expect(nessylandReader.purchaseRightToContent("99")).to.be.revertedWith( "Nessyland__ArticleDoesNotExist(99)")
+            })
+            it("Calculate Price less commission", async function () {
+                const {nessyland} = await loadFixture(readArticleFixture);
+                const result =  await nessyland.getPriceLessComission(parseEther("1"));
+                assert.equal(result.toString(), "970000000000000000")
+            })
+
+            it("Can Purchase Permission to read", async function () {
+                const {nessyland, nessylandReader, deployer, author, reader, gasSpentAthor} = await loadFixture(readArticleFixture);
+
+                const article = await nessyland.getArticleMetadata("0")
+                const tx = await nessylandReader.purchaseRightToContent("0", {value: article.price})
+                await tx.wait(1)
+
+                expect(await nessylandReader.hasArticleReadPermission("0")).to.be.true;
+            })
+
+            it("Pay correct amount to read article", async function () {
+                const {nessyland, nessylandReader, deployer, author, reader, gasSpentAthor} = await loadFixture(readArticleFixture);
+                // Balances Before 
+                expect(await reader.getBalance()).to.equal(parseEther("10000"))
+
+                // Transaction
+                const article = await nessyland.getArticleMetadata("0")
+                const tx = await nessylandReader.purchaseRightToContent("0", {value: article.price})
+                const readReceipt = await tx.wait(1)
+                const gasSpentReader = readReceipt.gasUsed.mul(readReceipt.effectiveGasPrice)
+
+                // Balances After
+                expect(await reader.getBalance()).to.equal(
+                    (parseEther("10000").sub(gasSpentReader)).sub(article.price))
+            })
+
+            it("Author become due the correct amount", async function () {
+                const {nessyland, nessylandReader, deployer, author, reader, gasSpentAthor} = await loadFixture(readArticleFixture);
+
+                const article = await nessyland.getArticleMetadata("0")
+                await nessylandReader.purchaseRightToContent("0", {value: article.price})
+
+                const nessylandAuthor = nessyland.connect(author);
+                const amountDue = await nessylandAuthor.getAmountDue();
+                const expected = article.price * 0.97;
+
+                assert.equal(amountDue, expected);
+            })
+        })
+
     })
+
+    "actual"
+    9999989850070184973858
+    9999999850070184973858
+    "expwcted"
+
+    -300000001130496
+    9700000000000000
+    "fees to author"
